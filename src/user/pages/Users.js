afb682426f0b1e1a7609d60c7509f7417c1316c8
jason-ubuntu <jason@luminesence.jp>
2023-03-06 00:26:33 -0800

    render user list & styling

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList.js';
+
 const Users = () => {
-    return <h2>Users Works!</h2>;
+  const USERS = [
+    {id:'u1', name:'Max Schwarz', image:"https://marketplace.canva.com/EAFEits4-uw/1/0/1600w/canva-boy-cartoon-gamer-animated-twitch-profile-photo-oEqs2yqaL8s.jpg", places:3}
+  ];
+  
+    return <UsersList items={USERS} />;
 };
 
 export default Users;
